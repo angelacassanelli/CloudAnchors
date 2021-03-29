cdf778f4cf6878b442a9baf7f618cba2fb968a9b
Angela Cassanelli <angelacassanelli98@gmail.com>
2021-03-29 13:44:14 +0200

    first commit

---
+++ Assets/FirebaseInit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+
+public class FirebaseInit : MonoBehaviour
+{    
+    protected bool isFirebaseInitialized = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);            
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available) {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                FirebaseApp app = FirebaseApp.DefaultInstance;
+                isFirebaseInitialized = true;                
+                Debug.Log ("Firebase is ready for use.");
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            } else {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+}
